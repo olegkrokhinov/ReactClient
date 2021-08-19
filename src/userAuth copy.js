dc1a3d8dd62154f1a09ec98368cd63f8e404b4c8
Oleg <oleg.krokhinov@gmail.com>
2021-08-19 21:58:18 +0300

    add exp jwt info nj userAuthcopy for learning .....

--- src/userAuth copy.js
+++ src/userAuth copy.js
@@ -1,64 +1,33 @@
 
-const URL_AUTH = "http://localhost:4000/auth/";
+https://gist.github.com/soulmachine/b368ce7292ddd7f91c15accccc02b8df
 
-
-const saveToLocalStorage = true; 
-
-export let userAccessToken = '';
-export let authenticatedUser = getUserFromLocalStorage();
-(authenticatedUser) && (userAccessToken = authenticatedUser.userAccessToken); 
-
-export function getAccessToken(){
-    return userAccessToken;
-}
-export function login(userLogin, userPassword) {
-    const body = {userLogin: userLogin, userPassword: userPassword};
-    return postUser('login', body, saveToLocalStorage);
-}
-
-export function register(userLogin, userPassword){
-    const body = {userLogin: userLogin, userPassword: userPassword};
-    return postUser('signup', body, !saveToLocalStorage);
-}
-
-export function logOut(){
-    const body = {userAccessToken: JSON.parse(localStorage.getItem('user')).userAccessToken};
-    localStorage.removeItem('user');
-    return postUser('logout', body, !saveToLocalStorage);
-}
-
-export function getUserFromLocalStorage(){
-    let user = JSON.parse(localStorage.getItem('user'));
-    user && accessTokenExpired(user) && localStorage.removeItem('user') && (user = {});
-    return user;
-}
-
-function accessTokenExpired(){
-  return false;
-}
-
-function saveUserToLocalStorage(user){
-    if (user.userAccessToken) {
-        localStorage.setItem("user", JSON.stringify(user));
+function getJWTExpireDate(jwtToken: string) {
+    if (jwtToken) {
+      try {
+        const [, payload] = jwtToken.split('.');
+        const { exp: expires } = JSON.parse(window.atob(payload));
+        if (typeof expires === 'number') {
+          return new Date(expires * 1000);
+        }
+      } catch {
+        // ignore
+      }
     }
-}
+    return null;
+  }
 
-function postUser(authPath, authBody, saveToLocalStorage = false){
-    return new Promise((resolve, reject)=>{
-        fetch(URL_AUTH + authPath, { 
-            method: 'POST', 
-            body: JSON.stringify(authBody),  
-            headers: { 
-                'Content-Type': 'application/json',                
-            }
-        })
-        .then(res => res.json())
-        .then(user => {
-            saveToLocalStorage && saveUserToLocalStorage(user);
-            authenticatedUser = user;
-            (authenticatedUser) && (userAccessToken = authenticatedUser.userAccessToken);
-            resolve(user);
-        })
-        .catch(reject);
-    });
-}
+
+jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) {
+        localStorage.clear();
+    }}
+});
+
+// Pass in function expiration date to check token 
+function checkToken(exp) {
+    if (Date.now() <= exp * 1000) {
+      console.log(true, 'token is not expired')
+    } else { 
+      console.log(false, 'token is expired') 
+    }
+  }
